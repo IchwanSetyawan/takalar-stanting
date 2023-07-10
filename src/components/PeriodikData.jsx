c93df698327a45c46766de0d078ec01696a38862
IchwanSetyawan <ichwansetyawan2015@gmail.com>
2023-07-10 21:51:53 +0700

    disable dropdown

--- src/components/PeriodikData.jsx
+++ src/components/PeriodikData.jsx
@@ -15,34 +15,21 @@ const PeriodikData = ({ children }) => {
         <div className="flex  justify-between items-center text-sm mb-8 mt-2">
           <div className=" flex mt-4 w-full justify-between items-center text-dark  gap-4">
             <select
+              disabled
               defaultValue="default"
               className="border-none shadow-smooth p-4 w-full text-md  rounded-lg "
             >
-              <option className="text-base" selected value="default" disabled>
-                Bulan
-              </option>
-              <option className="text-base" value="1">
-                Februari
-              </option>
-              <option className="text-base" value="2">
-                Agustus
+              <option className="text-base" selected value="default">
+                Juli
               </option>
             </select>
 
             <select
+              disabled={true}
               defaultValue="default"
               className="border-none p-4 w-full rounded-lg shadow-smooth"
             >
-              <option selected disabled className="text-base" lue="default">
-                Tahun
-              </option>
-              <option className="text-base" value="2">
-                2021
-              </option>
-              <option className="text-base" value="3">
-                2022
-              </option>
-              <option className="text-base" value="3">
+              <option selected className="text-base" value="">
                 2023
               </option>
             </select>
